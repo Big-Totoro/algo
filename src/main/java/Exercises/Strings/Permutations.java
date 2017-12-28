2a4e23afe7a4ffbe5d6cc97d2f76187523a22d31
Big-Totoro <34700230+Big-Totoro@users.noreply.github.com>
2017-12-28 12:55:12 +0600

    The comments were updated

--- src/main/java/Exercises/Strings/Permutations.java
+++ src/main/java/Exercises/Strings/Permutations.java
@@ -8,6 +8,12 @@ public class Permutations {
      *  Input: abc
      *  Output abc acb bac bca cab cba
      *
+     * The algorithm generates the permutations as follows:
+     *  Input: abc
+     *  Actions:
+     *  1) Get "b" and insert it to every position for "a". As result: "ba" "ab"
+     *  2) Get "c" and insert it to every position for "ab" and "ba". As result: cab, acb, abc, cba, bca, bac
+     *
      *  O(N) = N!
      *
      * @param input
@@ -17,8 +23,18 @@ public class Permutations {
         allPermutations(input, input.substring(0, 1), 1);
     }
 
+    /**
+     * Find the permutations for the specific substring
+     *
+     * @param original the original string
+     * @param input the substring to generate new combinations
+     * @param index of the next symbol to create combinations
+     */
     private static void allPermutations(String original, String input, int index) {
 
+        /**
+         * Stop and print as soon as we found the permutations for the whole string.
+         */
         if (index >= original.length()) {
 
             System.out.println(input);
@@ -26,8 +42,14 @@ public class Permutations {
             return;
         }
 
+        /**
+         * Get the symbol we will use to generate new combinations
+         */
         Character ch = original.charAt(index);
 
+        /**
+         * Generate new combinations with Symbol + Substring
+         */
         for (int i = 0; i <= input.length(); ++i) {
 
             StringBuilder builder = new StringBuilder(input);
@@ -47,6 +69,6 @@ public class Permutations {
 
     public static void main(String args[]) {
 
-        findAllPermutations("1234");
+        findAllPermutations("1234567");
     }
 }
