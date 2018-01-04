e5967ea106375ec5d230f76e077c76648f657e2a
Big-Totoro <34700230+Big-Totoro@users.noreply.github.com>
2018-01-04 16:39:12 +0600

    The two new permutation algos were added

--- src/main/java/Exercises/Strings/Permutations.java
+++ src/main/java/Exercises/Strings/Permutations.java
@@ -1,5 +1,9 @@
 package Exercises.Strings;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Permutations {
 
     /**
@@ -33,7 +37,7 @@ public class Permutations {
     private static void allPermutations(String original, String input, int index) {
 
         /**
-         * Stop and print as soon as we found the permutations for the whole string.
+         * Stop and print the string as soon as we found the permutations for the whole string.
          */
         if (index >= original.length()) {
 
@@ -67,8 +71,82 @@ public class Permutations {
         }
     }
 
+    /**
+     * Verifies if one string is the permutation of another string
+     *
+     * The algo sorts the both strings and then compare
+     *
+     * @param s1 the first string
+     * @param s2 the second string
+     * @return true in case of the first string is the permutation of the second string
+     */
+    public static boolean isPermutation1(String s1, String s2) {
+
+        assert(!s1.isEmpty());
+        assert(!s2.isEmpty());
+
+        if (s1.length() != s2.length()) {
+
+            return false;
+        }
+
+        char[] array1 = s1.toCharArray();
+        char[] array2 = s2.toCharArray();
+
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+
+        String r1 = new String(array1);
+        String r2 = new String(array2);
+
+        return r1.equalsIgnoreCase(r2);
+    }
+
+    public static boolean isPermutation2(String s1, String s2) {
+
+        assert(!s1.isEmpty());
+        assert(!s2.isEmpty());
+
+        if (s1.length() != s2.length()) {
+
+            return false;
+        }
+
+        Set<Character> set1 = new HashSet<Character>(s1.length());
+
+        /**
+         * Add the first string symbols to the set
+         */
+        for (char ch : s1.toCharArray()) {
+
+            set1.add(ch);
+        }
+
+        /**
+         * Verify all symbols of the first string exist in the second string
+         */
+        for (char ch : s2.toCharArray()) {
+
+            if (!set1.contains(ch)) {
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String args[]) {
 
-        findAllPermutations("1234567");
+        findAllPermutations("1234");
+
+        String s1 = "dog";
+        String s2 = "god";
+
+        boolean permutation1 = isPermutation1(s1, s2);
+        System.out.println(String.format("The '%s' string is permutation of the string '%s' - %s", s1, s2, Boolean.valueOf(permutation1)));
+
+        boolean permutation2 = isPermutation2(s1, s2);
+        System.out.println(String.format("The '%s' string is permutation of the string '%s' - %s", s1, s2, Boolean.valueOf(permutation2)));
     }
 }
